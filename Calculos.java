c58c99500a75fe04748e13612e8c1e479cc880c7
Jose-Prince <joseprince148mail.com>
2022-11-15 12:44:12 -0600

    Informe arreglos
    Se hicieron arreglos en el informe que se le muestra al usuario

--- Calculos.java
+++ Calculos.java
@@ -38,16 +38,16 @@ public Calculos(){
         for (Usuario usuario : usuarios) {
             int comparacion = (int) ((datos.getSalarioBase()/usuario.getSalario())*100);
             if (usuario.getSalario() < datos.getSalarioBase()){
-                compSalario = "Se pudo determinar que en base a su salario actual de Q." + usuario.getSalario() + 
+                compSalario = "\nSe pudo determinar que en base a su salario actual de Q." + usuario.getSalario() + 
                 "no excede el salario base establecido en Guatemala. \nUsted no excede este salario en un " + comparacion + 
                 "%.";
             } else if (usuario.getSalario() > datos.getSalarioBase()){
-                compSalario =  "Se pudo determinar que en base a su salario actual de Q." + usuario.getSalario() + 
+                compSalario =  "\nSe pudo determinar que en base a su salario actual de Q." + usuario.getSalario() + 
                 ", se excede el salario base establecido en Guatemala. \nUsted excede este salario en un " + comparacion + 
                 "%.";
             } else {
-                compSalario =  "Se pudo determinar que en base a su salario actual de Q." + usuario.getSalario() + 
-                "es igual al salario base establecido en Guatemala.";
+                compSalario =  "\nSe pudo determinar que en base a su salario actual de Q." + usuario.getSalario() + 
+                " es igual al salario base establecido en Guatemala.";
             }
         }
         return compSalario;
@@ -58,14 +58,14 @@ public Calculos(){
         for (Usuario usuario : usuarios) {
             double canastaSalario = Math.abs(usuario.getSalario()-datos.getPrecioCanBas());
             if (usuario.getSalario() >= datos.getPrecioCanBas()){
-                compCanastaBas = "Con su salario satisface exitosamente el precio de la canasta básica de Guatemala." +  
-                "Se ve que entre su salario y la canasta básica hay una diferencia de Q. " + canastaSalario + "." +
-                "Esto demuestra que su salario es capaz de satisfacer la canasta básica para una persona.";
+                compCanastaBas = " Con su salario satisface exitosamente el precio de la canasta básica de Guatemala." +  
+                "\nSe ve que entre su salario y la canasta básica hay una diferencia de Q. " + canastaSalario + "." +
+                " Esto demuestra que su salario es capaz de satisfacer la canasta básica para una persona.";
             } else {
-                compCanastaBas = "Con su salario no logra satisfacer el precio de la canasta básica de Guatemala." +  
-                "Se ve que entre su salario y la canasta básica hay una diferencia de Q. " + canastaSalario + "." +
-                "Esto demuestra que su salario no le alcanza ni para satisfacer la canasta básica para una persona.";
-            }
+                compCanastaBas = " Con su salario no logra satisfacer el precio de la canasta básica de Guatemala." +  
+                "\nSe ve que entre su salario y la canasta básica hay una diferencia de Q. " + canastaSalario + "." +
+                " Esto demuestra que su salario no le alcanza ni para satisfacer la canasta básica para una persona.";
+            } 
         }
         return compCanastaBas;
     }
@@ -83,7 +83,7 @@ public Calculos(){
     }
 
     public String gastosCompartidos(){
-        String gastoCompartido = "Con la ayuda financiera recibida, se tiene un capital total de Q." + capitalTotal();
+        String gastoCompartido = "\nCon la ayuda financiera recibida, se tiene un capital total de Q." + capitalTotal();
         return gastoCompartido;
     }
 
@@ -101,17 +101,17 @@ public Calculos(){
         for (Usuario usuario : usuarios) {
             double servicio = servicios();
             int porcentajeServicios = (int) ((servicio/usuario.getSalario())*100);
-            registroServicio = "En total se tienen gastos de Q." + servicio + " por higiene, gastos médicos y ocio.";
+            registroServicio = " En total se tienen gastos de Q." + servicio + " por higiene, gastos médicos y ocio.";
 
             if (usuario.getSalario() >= servicios()){
-                System.out.println("\nEsto representa un " + porcentajeServicios + "% de su salario.");
+                registroServicio += "\nEsto representa un " + porcentajeServicios + "% de su salario.";
                 if (porcentajeServicios >= 50){
                     registroServicio += "Se recomienda que trate de disminuir alguno de estos gastos si es posible porque\n" +
                     "Consumen gran parte de su salario. por favor disminuya lo antes que pueda estos gastos \nporque representan un riesgo para su vida.\n"
                     + ". De seguir con estos gastos tan altos puede\n llegar a tener problemas financieros a futuro.";
                 } else {
 
-                registroServicio += "Trate de mantenar sus gastos por esta cantidad, ya que no forma gran parte de su salario";
+                registroServicio += "Trate de mantenar sus gastos por esta cantidad, ya que no forma gran parte de su salario.";
                 }
             }
         }
@@ -123,10 +123,10 @@ public Calculos(){
         for (Usuario usuario : usuarios) {
             Vivienda estadoVivienda = usuario.getCasa();
             if (estadoVivienda.getAlquiler() == true){
-                viviendaRegistro += "Actualmente vive en una vivienda con alquiler, en donde el alquiler es de Q." + estadoVivienda.getGastoDomicilio() +
+                viviendaRegistro += " Actualmente vive en una vivienda con alquiler, en donde el alquiler es de Q." + estadoVivienda.getGastoDomicilio() +
                 ".\nEs recomendable que trate de conseguir una vivienda propia para no tener que depender del alquiler, además que \na largo plazo una propiedad propia conlleva menos gastos que pagando un alquiler por alojamiento.";
             } else {
-                viviendaRegistro += "Actualmente tiene una casa propia. Y se tienen gasto para la propiedad de Q. " + estadoVivienda.getGastoDomicilio() + ".";
+                viviendaRegistro += " Actualmente tiene una casa propia. \nY se tienen gasto para la propiedad de Q. " + estadoVivienda.getGastoDomicilio() + ".";
             }
         }
         return viviendaRegistro;
@@ -149,12 +149,12 @@ public Calculos(){
                     registroCarro += "Esta administrando bastante bien sus gastos en lo que respecta al consumo \nde gasolina y el uso de transporte público ya que este gasto no es significativo a comparación de su capital total.";
                 }
             } else {
-                registroCarro += "No existen gastos en gasolina pero se tiene que se gastan Q." + datos.getGastoTransporte()+ "en transportes. \nEste gasto representa el " + comparaGastoTransporte +
+                registroCarro += " No existen gastos en gasolina pero se tiene que se gastan Q." + datos.getGastoTransporte()+ " en transportes. \nEste gasto representa el " + comparaGastoTransporte +
                 "%, de su capital total.";
                 if (compararGastosTotalTransporte >= 50){
-                    registroCarro += "Trate de buscar medios de transportes más económicos, debido a que sus gastos en este ámbito están siendo demasiados gransdes\npara el capital se tiene disponible.";
+                    registroCarro += "\nTrate de buscar medios de transportes más económicos, debido a que sus gastos en este ámbito están siendo demasiados gransdes\npara el capital se tiene disponible.";
                 } else {
-                    registroCarro += "Su nivel de gasto entransportes no es excesivo y se mantiene en un buen porcentaje con respecto al capital total.";
+                    registroCarro += "\nSu nivel de gasto en transportes no es excesivo y se mantiene en un buen porcentaje con respecto al capital total.";
                 }
             }
         }
